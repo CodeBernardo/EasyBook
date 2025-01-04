54cc80e288200bedaff6832fe4a3f2a49f0e7cf6
Bernardo Stein <stein.bernardo@proton.me>
2025-01-04 14:37:50 -0300

    feat: lib installs

---
+++ easybooking/prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: false,
+  trailingComma: "none",
+  tabWidth: 2,
+  arrowParens: "always",
+  semi: false,
+  printWidth: 80
+}
